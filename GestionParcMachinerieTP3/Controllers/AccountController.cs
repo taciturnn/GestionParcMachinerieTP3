891dfdbced0a773dcabe63391f22c0874f56fc88
taciturnn <taciturnn@gmail.com>
2019-11-27 18:12:21 -0500

    WIP. Trying to add role management.

--- GestionParcMachinerieTP3/Controllers/AccountController.cs
+++ GestionParcMachinerieTP3/Controllers/AccountController.cs
@@ -17,6 +17,7 @@ namespace GestionParcMachinerieTP3.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        private ApplicationRoleManager _roleManager;
 
         public AccountController()
         {
--- GestionParcMachinerieTP3/Models/IdentityModels.cs
+++ GestionParcMachinerieTP3/Models/IdentityModels.cs
@@ -18,6 +18,12 @@ namespace GestionParcMachinerieTP3.Models
         }
     }
 
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() : base() { }
+        public ApplicationRole(string roleName) : base(roleName) { }
+    }
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
@@ -29,5 +35,6 @@ namespace GestionParcMachinerieTP3.Models
         {
             return new ApplicationDbContext();
         }
+
     }
 }

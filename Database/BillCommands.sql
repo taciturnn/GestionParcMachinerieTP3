b34e77911b9fd3d72719dad6f5ff13a9fb095e2c
Q5h3gf <sam.android.sg@gmail.com>
2019-12-04 21:40:12 -0500

    ajout de la table BillCommands

---
+++ Database/BillCommands.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BillCommands]
+(
+	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
+    [CommandId] INT NOT NULL, 
+    [BillId] INT NOT NULL, 
+	CONSTRAINT [FK_dbo.BillCommands_dbo.Commands_CommandId] FOREIGN KEY ([CommandId]) REFERENCES [dbo].[Commands] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_dbo.BillCommands_dbo.Bills_BillId] FOREIGN KEY ([BillId]) REFERENCES [dbo].[Bills] ([Id]) ON DELETE CASCADE
+)
---
+++ GestionParcMachinerieTP3/Models/Db/BillCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestionParcMachinerieTP3.Models
+{
+    public partial class BillCommands
+    {
+        public int BillCommandsID { get; set; }
+        public int CommandID { get; set; }
+        public int BillID { get; set; }
+
+        public virtual Command Command { get; set; }
+        public virtual Bill Bill { get; set; }
+    }
+}

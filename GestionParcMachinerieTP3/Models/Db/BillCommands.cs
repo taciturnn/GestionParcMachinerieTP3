c93d34732bbe71aeb0a38b1976cb691aebb00253
Q5h3gf <sam.android.sg@gmail.com>
2019-12-05 00:32:02 -0500

    correction des noms de colonne

--- GestionParcMachinerieTP3/Models/Db/BillCommands.cs
+++ GestionParcMachinerieTP3/Models/Db/BillCommands.cs
@@ -5,9 +5,9 @@ namespace GestionParcMachinerieTP3.Models
 {
     public partial class BillCommands
     {
-        public int BillCommandsID { get; set; }
-        public int CommandID { get; set; }
-        public int BillID { get; set; }
+        public int Id { get; set; }
+        public int CommandId { get; set; }
+        public int BillId { get; set; }
 
         public virtual Command Command { get; set; }
         public virtual Bill Bill { get; set; }
